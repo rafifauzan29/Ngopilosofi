96c3c20d20d572c7720bd807a7cd168fe7790b64
rafifauzan29 <rafifauzan1597@gmail.com>
2025-05-29 00:14:34 +0700

    revisi dikit

--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -37,6 +37,7 @@ router.post('/register', async (req, res) => {
         id: newUser._id,
         name: newUser.name,
         email: newUser.email,
+        avatar: newUser.avatar,
         role: newUser.role
       }
     });
@@ -77,6 +78,7 @@ router.post('/login', async (req, res) => {
         id: user._id,
         name: user.name,
         email: user.email,
+        avatar: user.avatar, 
         role: user.role
       }
     });
